bd16c78b231445fa66f266c3468d561da1bc5b98
Tunglies <Tunglies@outlook.com>
2023-01-30 06:39:05 +0800

    update: script

--- script.sh
+++ script.sh
@@ -1,14 +1,12 @@
-echo "" > all.txt
-
 for file in $(ls -tr animeTrackerList/*.txt |grep -i -v aria);
 do
-   (cat $file | sort | uniq | grep -v "^$" | grep -v "#" | grep -i -E ^"http|udp"; echo)  >> all.txt
+   (cat $file | sort | uniq | grep -v "^$" | grep -v "#" | grep -i -E ^"http|udp"; echo)  >> temp.txt
 done
 
 
 for file in $(ls -tr TrackersListCollection/*.txt |grep -i -v aria);
 do
-   (cat $file | sort | uniq | grep -v "^$" | grep -v "#" | grep -i -E ^"http|udp"; echo)  >> all.txt
+   (cat $file | sort | uniq | grep -v "^$" | grep -v "#" | grep -i -E ^"http|udp"; echo)  >> temp.txt
 done
 
-cat all.txt | sort | uniq > all.txt
+cat temp.txt | sort | uniq > all.txt
