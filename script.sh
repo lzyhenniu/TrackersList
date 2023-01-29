468b530732bd3b2e883bbdb65281606012825fd3
Tunglies <Tunglies@outlook.com>
2023-01-30 05:45:21 +0800

    update: script

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
-echo "Hello World!"
-
 for file in $(ls -tr animeTrackerList/*.txt |grep -i -v aria);
 do
    (cat $file | sort | uniq | grep -v "^$" | grep -v "#" | grep -i -E ^"http|udp"; echo)  >> all.txt
