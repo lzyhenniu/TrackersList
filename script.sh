0fc4d6f5e59d4d9d95cd3ae886d8d0ea6cf5e513
Tunglies <Tunglies@outlook.com>
2023-01-30 05:07:13 +0800

    update: script

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
-# git clone https://github.com/DeSireFire/animeTrackerList
-for file in $(ls -tr ./animeTrackerList/*.txt |grep -i -v aria);
+for file in $(ls -tr animeTrackerList/*.txt |grep -i -v aria);
 do
    (cat $file | sort | uniq | grep -v "^$" | grep -v "#" | grep -i -E ^"http|udp"; echo)  >> all.txt
 done
 
 
-# git clone https://github.com/XIU2/TrackersListCollection
-for file in $(ls -tr ./TrackersListCollection/*.txt |grep -i -v aria);
+for file in $(ls -tr TrackersListCollection/*.txt |grep -i -v aria);
 do
    (cat $file | sort | uniq | grep -v "^$" | grep -v "#" | grep -i -E ^"http|udp"; echo)  >> all.txt
 done
